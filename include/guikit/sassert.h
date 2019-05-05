342c70976ccfda99de3bd1423f9d9b1bd2a09ea8
Jaeden Amero <jaeden@patater.com>
2019-05-05 07:52:06 +0100

    sassert: Add a STATIC_ASSERT macro

---
+++ include/guikit/sassert.h
@@ -0,0 +1,16 @@
+/*
+ *  sassert.h
+ *  Patater GUI Kit
+ *
+ *  Created by Jaeden Amero on 2019-05-05.
+ *  Copyright 2019. SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef SASSERT_H
+#define SASSERT_H
+
+#define STATIC_ASSERT(x, msg) \
+    extern int STATIC_ASSERT_##msg[1]; \
+    extern int STATIC_ASSERT_##msg[(x)?1:2]
+
+#endif
