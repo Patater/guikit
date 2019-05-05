dc5dfc0e82cb3743e6a0520a62b355ecbd81eff0
Jaeden Amero <jaeden@patater.com>
2019-05-05 17:47:35 +0100

    test: Add a test runner
    Add a top-level test runner that can run tests in multiple modules.

---
+++ tests/test.c
@@ -0,0 +1,41 @@
+/*
+ *  test.c
+ *  Patater GUI Kit
+ *
+ *  Created by Jaeden Amero on 2019-05-05.
+ *  Copyright 2019. SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "test.h"
+#include <stdio.h>
+
+#define PUTT_TEST_FAIL 125
+#define PUTT_TEST_PASS 0
+
+/* The list of tests will come from another file. We just run them. The list is
+ * an array that is zero-terminated, so we know there should be at least one
+ * element. */
+extern const test_fn tests[1];
+
+int main()
+{
+    int ret;
+    int tests_run = 0;
+    int tests_passed = 0;
+    const test_fn *list = tests;
+
+    while (*list)
+    {
+        ret = list[0]();
+        if (ret >= 0)
+        {
+            ++tests_passed;
+        }
+        ++tests_run;
+        ++list;
+    }
+
+    printf("%d of %d tests passed.\n", tests_passed, tests_run);
+
+    return tests_passed == tests_run ? PUTT_TEST_PASS : PUTT_TEST_FAIL;
+}
