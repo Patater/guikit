172b0e1bf2789eef96e962967fe6cae9a870378a
Jaeden Amero <jaeden@patater.com>
2020-12-14 03:52:00 +0000

    graphics: Add line drawing
    Add generic line drawing functions as well as special-case line drawing
    functions that have a quicker implementation by way of assuming line
    form (diagonal, horizontal, vertical).

---
+++ apps/lines/lines.c
@@ -0,0 +1,75 @@
+/*
+ *  lines.c
+ *  Patater GUI Kit
+ *
+ *  Created by Jaeden Amero on 2020-12-13.
+ *  Copyright 2021. SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "guikit/graphics.h"
+#include "guikit/panic.h"
+#include <stdio.h>
+
+void VectorsUp(int color, int x0, int y0, int xlen, int ylen)
+{
+    int x;
+    int y;
+
+    /* Lines from center to top of rectangle */
+    x = x0 - xlen;
+    y = y0 - ylen;
+    for(; x < x0 + xlen; x++)
+    {
+        DrawLine(color, x0, y0, x, y);
+    }
+    /* Lines from center to right of rectangle */
+    x = x0 + xlen - 1;
+    y = y0 - ylen;
+    for(; y < y0 + ylen; y++)
+    {
+        DrawLine(color, x0, y0, x, y);
+    }
+    /* Lines from center to bottom of rectangle */
+    x = x0 + xlen - 1;
+    y = y0 + ylen - 1;
+    for(; x >= x0 - xlen; x--)
+    {
+        DrawLine(color, x0, y0, x, y);
+    }
+    /* Lines from center to left of rectangle */
+    x = x0 - xlen;
+    y = y0 + ylen - 1;
+    for(; y >= y0 - ylen; y--)
+    {
+        DrawLine(color, x0, y0, x, y);
+    }
+}
+
+void TestDrawLines(void)
+{
+    enum { X_MAX = 640, Y_MAX = 480 };
+
+    VectorsUp(COLOR_BLUE, X_MAX / 4, Y_MAX / 4, X_MAX / 4, Y_MAX / 4);
+    VectorsUp(COLOR_GREEN, X_MAX * 3 / 4, Y_MAX / 4, X_MAX / 4, Y_MAX / 4);
+    VectorsUp(COLOR_LIGHT_BLUE, X_MAX / 4, Y_MAX * 3 / 4, X_MAX / 4, Y_MAX / 4);
+    VectorsUp(COLOR_RED, X_MAX * 3 / 4, Y_MAX * 3 / 4, X_MAX / 4, Y_MAX / 4);
+}
+
+int main()
+{
+    int ret;
+
+    ret = InitGraphics();
+    if (ret < 0)
+    {
+        panic("Couldn't init graphics\n");
+    }
+
+    printf("Drawing loads of lines...\n");
+    TestDrawLines();
+    FreeGraphics();
+
+    printf("Goodbye.\n");
+
+    return 0;
+}
