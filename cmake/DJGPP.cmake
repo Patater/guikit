a8afa871aeb1fc07ea9287479a318c07f68ad2e6
Jaeden Amero <jaeden@patater.com>
2021-04-05 12:17:56 +0100

    graphics: Add graphics library
    Add a graphics header with the basics for initializing a display,
    filling it with a color, and displaying the result. Also add an
    implementation for SDL2.

--- cmake/DJGPP.cmake
+++ cmake/DJGPP.cmake
@@ -3,3 +3,6 @@ set(triple i586-pc-msdosdjgpp-gcc)
 set(CMAKE_C_COMPILER ${triple})
 set(CMAKE_CXX_COMPILER ${triple})
 set(CMAKE_CROSSCOMPILING_EMULATOR "dosbox")
+
+# GUI Kit configuration
+set(GRAPHICS "VGA" CACHE STRING "Graphics rendering backend")
---
+++ include/guikit/graphics.h
@@ -0,0 +1,54 @@
+/*
+ *  graphics.h
+ *  Patater GUI Kit
+ *
+ *  Created by Jaeden Amero on 2020-12-12.
+ *  Copyright 2020. SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <stddef.h>
+
+/* VGA resolution (in Mode 0x12) */
+enum {
+    SCREEN_WIDTH = 640,
+    SCREEN_HEIGHT = 480
+};
+
+/* Standard Macintosh palette */
+enum {
+    COLOR_BLACK = 0,
+    COLOR_DARK_GRAY = 1,
+    COLOR_GRAY = 2,
+    COLOR_LIGHT_GRAY = 3,
+    COLOR_LIGHT_BROWN = 4,
+    COLOR_BROWN = 5,
+    COLOR_DARK_GREEN = 6,
+    COLOR_GREEN = 7,
+    COLOR_LIGHT_BLUE = 8,
+    COLOR_BLUE = 9,
+    COLOR_PURPLE = 10,
+    COLOR_PINK = 11,
+    COLOR_RED = 12,
+    COLOR_ORANGE = 13,
+    COLOR_YELLOW = 14,
+    COLOR_WHITE = 15,
+    NUM_COLORS
+};
+
+int InitGraphics(void);
+void FreeGraphics(void);
+
+/* Some graphics implementations render to a buffer and then copy to the screen
+ * all at once. Some other (potentially flickery) implementations render
+ * directly to the screen in real-time, and for these implementations this
+ * function may not do too much. */
+void ShowGraphics(void);
+
+void SetColor(int color);
+
+void FillScreen(int color);
+
+#endif
