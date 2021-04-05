0cc098b68cf16de4b462ee8d2bb42b83fce783b7
Jaeden Amero <jaeden@patater.com>
2021-04-05 10:01:04 +0100

    gcovr: Print summary of coverage

--- scripts/gcovr.sh
+++ scripts/gcovr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 mkdir -p build/gcovr
-(cd build && gcovr -r .. --gcov-executable llvm-gcov.sh --html --html-details -o gcovr/index.html)
+(cd build && gcovr -s -r .. --gcov-executable llvm-gcov.sh --html --html-details -o gcovr/index.html)
