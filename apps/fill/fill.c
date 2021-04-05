d5265d922cc91c3078504f40babb1a044b753840
Jaeden Amero <jaeden@patater.com>
2021-04-05 12:20:44 +0100

    apps: Add simple fill screen example
    Show how to use the graphics library to display a blank screen.

---
+++ apps/fill/fill.c
@@ -0,0 +1,35 @@
+/*
+ *  fill.c
+ *  Patater GUI Kit
+ *
+ *  Created by Jaeden Amero on 2021-04-05.
+ *  Copyright 2021. SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "guikit/graphics.h"
+#include "guikit/panic.h"
+#include <stdio.h>
+
+int main()
+{
+    int ret;
+    int i;
+
+    ret = InitGraphics();
+    if (ret < 0)
+    {
+        panic("Couldn't init graphics\n");
+    }
+
+    printf("Hello world\n");
+
+    /* Display a black screen for about 2 seconds. */
+    for(i = 0; i < 60 * 2; ++i)
+    {
+        FillScreen(COLOR_BLACK);
+        ShowGraphics();
+    }
+    FreeGraphics();
+
+    return 0;
+}
