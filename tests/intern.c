40b83b4a5f376700ef41cdc1883525f9e66bed93
Jaeden Amero <jaeden@patater.com>
2021-08-29 19:04:39 +0100

    test: intern: Clean up at end of test
    We were calling intern_free() in the beginning of tests to ensure the
    tests each started with a clean slate. Refactor the tests so that they
    clean up after themselves instead, leaving the string interning state
    ready for new tests. This helps avoid LeakSanitizer errors on Linux by
    having no strings interned on exit.

--- tests/intern.c
+++ tests/intern.c
@@ -11,8 +11,6 @@
 
 static int test_initial_state(void)
 {
-    intern_free();
-
     TEST_EQ(intern_num_hits(), 0);
     TEST_EQ(intern_num_interned(), 0);
 
@@ -23,14 +21,14 @@ static int test_after_first_string_added(void)
 {
     const char *oldstr1;
 
-    intern_free();
-
     /* Add first string. */
     oldstr1 = intern("hello");
 
     TEST_EQ(intern_num_hits(), 0);
     TEST_EQ(intern_num_interned(), 1);
     TEST_EQS(oldstr1, "hello");
+
+    intern_free();
 
     return 0;
 }
@@ -39,8 +37,6 @@ static int test_after_same_string_added(void)
 {
     const char *oldstr1;
     const char *newstr1;
-
-    intern_free();
 
     /* Add same string. */
     oldstr1 = intern("hello");
@@ -51,6 +47,8 @@ static int test_after_same_string_added(void)
     TEST_EQS(newstr1, "hello");
     TEST_EQP(newstr1, oldstr1);
 
+    intern_free();
+
     return 0;
 }
 
@@ -59,8 +57,6 @@ static int test_after_different_string_added(void)
     const char *oldstr1;
     const char *newstr1;
     const char *newstr2;
-
-    intern_free();
 
     /* Add different string. */
     oldstr1 = intern("hello");
@@ -73,6 +69,8 @@ static int test_after_different_string_added(void)
     TEST_EQS(newstr2, "world");
     TEST_NEP(newstr2, oldstr1);
 
+    intern_free();
+
     return 0;
 }
 
@@ -81,13 +79,13 @@ static int test_copy_semantics(void)
     static const char *base = "hello";
     const char *copy;
 
-    intern_free();
-
     /* Add a string */
     copy = intern(base);
 
     /* Ensure that the copy returned is different memory than the base */
     TEST_NEP(copy, base);
+
+    intern_free();
 
     return 0;
 }
@@ -95,8 +93,6 @@ static int test_copy_semantics(void)
 static int test_copy_semantics_value(void)
 {
     const char *copy;
-
-    intern_free();
 
     {
         char base[] = "hello";
@@ -110,6 +106,8 @@ static int test_copy_semantics_value(void)
     /* Ensure that the copy is a true copy. This may crash if the memory used
      * for the base went out of scope. */
     TEST_EQS(copy, "hello");
+
+    intern_free();
 
     return 0;
 }
