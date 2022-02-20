c0a710ae351956232d6c1db40184bb4334a94640
Jaeden Amero <jaeden@patater.com>
2022-02-20 15:15:15 +0000

    guikit: Enable include-what-you-use
    Enable running include-what-you-use on the GUI Kit library.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cmake -S . -B build -DENABLE_SANITIZERS=1 -DENABLE_COVERAGE=1 -DENABLE_WARNINGS=1 -DCMAKE_BUILD_TYPE=Debug -GNinja
+cmake -S . -B build -DENABLE_SANITIZERS=1 -DENABLE_COVERAGE=1 -DENABLE_WARNINGS=1 -DENABLE_IWYU=1 -DCMAKE_BUILD_TYPE=Debug -GNinja
 cmake --build build
 cmake --build build --target test
 #test-runner -v -j 7 -p build/tests
