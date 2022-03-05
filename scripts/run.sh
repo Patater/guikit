3025acc296c6784dd628698deda3470eba78478f
Jaeden Amero <jaeden@patater.com>
2022-03-05 15:41:52 +0000

    scripts: Add a script to run all apps
    As each app outputs a screenshot bitmap, this will end up creating all
    the bitmaps. We then convert them to png with ImageMagick.

---
+++ scripts/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+./build/apps/bezline/bezline
+./build/apps/bmpshow/bmpshow
+./build/apps/cardioid/cardioid
+./build/apps/circles/circles
+./build/apps/fill/fill
+./build/apps/fonts/fonts
+./build/apps/histgram/histgram
+./build/apps/lines/lines
+./build/apps/rect/rect
+./build/apps/roundrct/roundrct
+./build/apps/waggle/waggle
+
+convert bezline.bmp images/bezline.png
+convert bmpshow.bmp images/bmpshow.png
+convert cardioid.bmp images/cardioid.png
+convert circles.bmp images/circles.png
+convert fill.bmp images/fill.png
+convert fonts.bmp images/fonts.png
+convert histgram.bmp images/histgram.png
+convert lines.bmp images/lines.png
+convert rect.bmp images/rect.png
+convert roundrct.bmp images/roundrct.png
+convert waggle.bmp images/waggle.png
